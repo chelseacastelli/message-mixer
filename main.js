6f45ba524445f8c3d073a29d7c36ffecb4cf6b21
chelseacastelli <chel.castel@aol.com>
2021-06-15 10:42:03 -0400

    randomly walk through message components to create sentence

--- main.js
+++ main.js
@@ -3,35 +3,74 @@
 const sentenceCatalog = {
     "Sentences": {
         "You": {
-            "verb": ['are', 'can', 'have', 'should', 'love', 'will'],
+            "verb": ['hold', 'ate', 'have', 'launched', 'love', 'will'],
             "number": [1, 4, 6, 12, 2, 0],
-            "noun": ['socks', 'superheros', 'monkeys', 'baseball gloves', 'raspberrys'],
+            "adjective": ['hairy', 'smelly', 'tall', 'cuddly', 'moist'],
+            "noun": ['socks', 'superheros', 'monkeys', 'baseball mitts', 'raspberrys'],
         },
         "You're": {
-            "verb": ['having', 'walking', 'snoring', 'dodging'],
+            "verb": ['holding', 'walking', 'snoring', 'dodging'],
             "number": [1, 4, 6, 12, 2, 0],
-            "noun": ['cakes', 'phones', 'pillows', 'sandals']
+            "adjective": ['heavy', 'slow', 'tired', 'smart'],
+            "noun": ['earmuffs', 'beepers', 'pillows', 'sandals']
         },
         "I": {
             "verb": ['told', 'worked', 'called', 'tried', 'sniffed'],
             "number": [1, 4, 6, 12, 2, 0],
-            "noun": ['stickers', 'belts', 'hoses', 'pomegranate seeds', 'spine-broken books']
+            "adjective": ['long', 'cold', 'pretty', 'gigantic'],
+            "noun": ['stickers', 'belts', 'shovels', 'pomegranate seeds', 'spine-broken books']
         },
         "They": {
             "verb": ['stole', 'smells', 'knitted', 'sold', 'hung'],
             "number": [1, 4, 6, 12, 2, 0, 54],
+            "adjective": ['bouncing', 'icky', 'wet', 'blinding'],
             "noun": ['desks', 'alarm clocks', 'pool floats', 'vinyl records']
         },
         "He": {
             "verb": ['owns', 'cursed', 'played', 'dug'],
             "number": [1, 4, 6, 12, 2, 0],
-            "noun": ['napkins', 'chargers', 'straws', 'scones']
+            "adjective": ['obedient', 'lazy', 'microscopic', 'chubby'],
+            "noun": ['napkins', 'chargers', 'straws', 'croissants']
         },
         "She": {
             "verb": ['wipes', 'swung', 'moonwalked', 'cleaned', 'berates'],
             "number": [1, 4, 6, 12, 2, 0],
-            "noun": ['canteloupes', 'ostriches', 'camping tents', 'pianos', 'bushes']
+            "adjective": ['flabby', 'worried', 'zealous', 'gentle'],
+            "noun": ['watermelons', 'ostriches', 'camping tents', 'pianos', 'bushes']
         }
     }
-
 }
+
+/**
+ * Randomly walk through object to create sentence
+ */
+const randomWalk = () => {
+    let subject = Object.keys(sentenceCatalog["Sentences"]);
+    subject = subject[Math.floor(Math.random() * subject.length)];
+
+    let verb = sentenceCatalog["Sentences"][subject]["verb"];
+    verb = verb[Math.floor(Math.random() * verb.length)];
+
+    let number = sentenceCatalog["Sentences"][subject]["number"];
+    number = number[Math.floor(Math.random() * number.length)];
+
+    let adjective = sentenceCatalog["Sentences"][subject]["adjective"];
+    adjective = adjective[Math.floor(Math.random() * adjective.length)];
+
+    let noun = sentenceCatalog["Sentences"][subject]["noun"];
+    noun = noun[Math.floor(Math.random() * noun.length)];
+
+    // Change noun from plural to singular when referring to 1 instance of it
+    if (number === 1 && noun[noun.length-1] === 's') { 
+        let cut = 1;
+        if (noun[noun.length-2] == 'e'){
+            cut += 1;
+        }
+
+        noun = noun.slice(0, noun.length-cut);
+    }
+
+    console.log(subject, verb, String(number), adjective, noun);
+}
+
+randomWalk();
