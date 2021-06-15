5e78e7f03f5418e8f8508d74903fb97ddd571c88
chelseacastelli <chel.castel@aol.com>
2021-06-15 10:18:10 -0400

    create object to store message components

---
+++ main.js
@@ -0,0 +1,37 @@
+
+
+const sentenceCatalog = {
+    "Sentences": {
+        "You": {
+            "verb": ['are', 'can', 'have', 'should', 'love', 'will'],
+            "number": [1, 4, 6, 12, 2, 0],
+            "noun": ['socks', 'superheros', 'monkeys', 'baseball gloves', 'raspberrys'],
+        },
+        "You're": {
+            "verb": ['having', 'walking', 'snoring', 'dodging'],
+            "number": [1, 4, 6, 12, 2, 0],
+            "noun": ['cakes', 'phones', 'pillows', 'sandals']
+        },
+        "I": {
+            "verb": ['told', 'worked', 'called', 'tried', 'sniffed'],
+            "number": [1, 4, 6, 12, 2, 0],
+            "noun": ['stickers', 'belts', 'hoses', 'pomegranate seeds', 'spine-broken books']
+        },
+        "They": {
+            "verb": ['stole', 'smells', 'knitted', 'sold', 'hung'],
+            "number": [1, 4, 6, 12, 2, 0, 54],
+            "noun": ['desks', 'alarm clocks', 'pool floats', 'vinyl records']
+        },
+        "He": {
+            "verb": ['owns', 'cursed', 'played', 'dug'],
+            "number": [1, 4, 6, 12, 2, 0],
+            "noun": ['napkins', 'chargers', 'straws', 'scones']
+        },
+        "She": {
+            "verb": ['wipes', 'swung', 'moonwalked', 'cleaned', 'berates'],
+            "number": [1, 4, 6, 12, 2, 0],
+            "noun": ['canteloupes', 'ostriches', 'camping tents', 'pianos', 'bushes']
+        }
+    }
+
+}
